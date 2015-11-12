d95ef2d2a5824263f11dc2cbdb0ae0b458ccbcce
Ryan Bird <dawgillac11@yahoo.com>
2015-11-12 14:32:46 -0800

    working again!

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
- # request, redirect, session, flash  # Import Flask to allow us to create our app and render_template to allow us to render index.html
+from flask import Flask, render_template, request, redirect, session, flash  # Import Flask to allow us to create our app and render_template to allow us to render index.html
 app = Flask(__name__)
-# app.secret_key = "ThisIsSecret"
+app.secret_key = "ThisIsSecret"
 
 @app.route('/') # The "@" symbol designates a "decorator" which attaches the following function to the '/' route. This means that whenever we send a request to localhost:5000/ we will run the following "hello_world" function.
 def index():
